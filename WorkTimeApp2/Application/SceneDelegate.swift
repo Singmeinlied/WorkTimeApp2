06916cc299e7333e937796f8bc2cbe7e0401751a
Singmeinlied <98156702+Singmeinlied@users.noreply.github.com>
2024-01-21 19:48:11 +0600

    Initial commit

---
+++ WorkTimeApp2/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  WorkTimeApp2
+//
+//  Created by Abai Kush on 21/1/24.
+//
+
+import UIKit
+
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ WorkTimeApp2/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WorkTimeApp2
+//
+//  Created by Abai Kush on 21/1/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+}
